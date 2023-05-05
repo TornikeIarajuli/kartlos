7229e9719db0f58b1f3c7e893a907e53ffe6fff1
TokoIarajuli <tiarajuli7899@sdsu.edu>
2023-05-05 16:14:30 +0400

    reduced file number

--- src/Components/ShippingForm.jsx
+++ src/Components/ShippingForm.jsx
@@ -2,16 +2,12 @@ import React, { useState } from "react";
 import "./shipping.css";
 
 function ShippingForm(props) {
-  // const [countryId, setCountryId] = useState(0);
   const [city, setCity] = useState("");
-  // const [state, setState] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [address1, setAddress1] = useState("");
-  // const [address2, setAddress2] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  // const [postalCode, setPostalCode] = useState("");
   const [redirectUrl, setRedirectUrl] = useState("");
 
   const handleSubmit = (e) => {
@@ -100,14 +96,6 @@ function ShippingForm(props) {
           onChange={(e) => setAddress1(e.target.value)}
         />
 
-        {/* <label htmlFor="address2">მეორე მისამართი:</label>
-        <input
-          type="text"
-          id="address2"
-          value={address2}
-          onChange={(e) => setAddress2(e.target.value)}
-        /> */}
-
         <label htmlFor="city">ქალაქი:</label>
         <input
           type="text"
@@ -115,30 +103,6 @@ function ShippingForm(props) {
           value={city}
           onChange={(e) => setCity(e.target.value)}
         />
-
-        {/* <label htmlFor="state">რეგიონი:</label>
-        <input
-          type="text"
-          id="state"
-          value={state}
-          onChange={(e) => setState(e.target.value)}
-        />
-
-        <label htmlFor="postalCode">საფოსტო კოდი:</label>
-        <input
-          type="text"
-          id="postalCode"
-          value={postalCode}
-          onChange={(e) => setPostalCode(e.target.value)}
-        /> */}
-
-        {/* <label htmlFor="countryId">ქვეყანა:</label>
-        <input
-          type="text"
-          id="countryId"
-          value={countryId}
-          onChange={(e) => setCountryId(e.target.value)}
-        /> */}
 
         <label htmlFor="phone">ნომერი:</label>
         <input
